31d21e1fdf7c9087731940a134b7c355661ad055
ravi.dhakal <freakin-ravi@freakin-ravi@gmail.com>
2025-09-13 22:32:48 +0545

    database loading

--- apps/mysite/settings.py
+++ apps/mysite/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
-#}
+# }
 
 DATABASES = {
     "default": {
